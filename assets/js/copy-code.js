26351ec0f7e27b6a3c0f5e4d93e1d897f13a24e7
thorstenstoehr <thorsten.stoehr@gmail.com>
2025-02-28 15:06:06 +0100

    modified:   assets/js/copy-code.js

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -5,12 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const button = document.createElement("button");
     button.classList.add("copy-button");
-    button.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-      </svg>
-    `;
+    button.innerHTML = `<i class="fa-regular fa-copy"></i>`;
 
     const pre = codeBlock.parentNode;
     container.appendChild(pre.cloneNode(true));
@@ -21,18 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
       const codeText = codeBlock.innerText || codeBlock.textContent;
 
       navigator.clipboard.writeText(codeText).then(() => {
-        button.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <polyline points="20 6 9 17 4 12"></polyline>
-          </svg>
-        `;
+        button.innerHTML = `<i class="fa-solid fa-check"></i>`;
         setTimeout(() => {
-          button.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-              <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-            </svg>
-          `;
+          button.innerHTML = `<i class="fa-regular fa-copy"></i>`;
         }, 2000);
       }).catch(err => {
         console.error("Copy failed", err);
@@ -40,3 +26,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
